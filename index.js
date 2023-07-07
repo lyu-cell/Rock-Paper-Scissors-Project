d216a3784e21752879386408aaa66c181360236c
lyu-cell <shizueizawa340@gmail.com>
2023-07-07 17:04:24 +0600

    New function for comparing the two inputs and announcing the winner is added

--- index.js
+++ index.js
@@ -3,10 +3,33 @@ let randomNum = Math.floor(Math.random() * 3) + 1;
 
 function getComputerchoice() {
     if (randomNum === 1) {
-        console.log("Rock");
+        return "rock"
     } else if (randomNum === 2) {
-        console.log("Paper");
+        return "paper"
     } else {
-        console.log("Scissor")
+       return "scissor"
     }
 }
+
+let playerselection = prompt("rock, paper or scissor?");
+let computerSelection = getComputerchoice();
+
+console.log(computerSelection);
+console.log(playerselection);
+
+function playRound(playerselection, computerSelection) {
+    if (playerselection === "rock" && computerSelection === "scissor" ||
+        playerselection === "paper" && computerSelection === "rock" ||
+        playerselection === "scissor" && computerSelection === "paper") {
+        return `You won! ${playerselection} beats ${computerSelection}`
+    } else if (playerselection === "rock" && computerSelection === "rock" ||
+               playerselection === "paper" && computerSelection === "paper" ||
+               playerselection === "scissor" && computerSelection === "scissor") {
+        return `it's a draw! ${playerselection} and ${computerSelection} are the same!`
+    } else {
+        return `You Lost! ${computerSelection} beats ${playerselection}`
+    }
+}
+
+console.log(playRound(playerselection, computerSelection));
+
