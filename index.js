0a8fbf178f58f011a59ba15dbb4f00d1f7f25a51
lyu-cell <shizueizawa340@gmail.com>
2023-07-21 23:45:00 +0600

    Added the score comparsion and winner announcing function

--- index.js
+++ index.js
@@ -29,21 +29,12 @@ function playRound(playerselection, computerSelection) {
     if (playerselection === "rock" && computerSelection === "scissor" ||
         playerselection === "paper" && computerSelection === "rock" ||
         playerselection === "scissor" && computerSelection === "paper") {
-            // if (player >= 2 || player >= 3) {
-            //     console.log("Congratulation! You have Won!");
-            // }
         return `You Won! ${playerselection} Beats ${computerSelection} (Player Score ${player = p++})`;  
     } else if (playerselection === "rock" && computerSelection === "rock" ||
                playerselection === "paper" && computerSelection === "paper" ||
-               playerselection === "scissor" && computerSelection === "scissor") {
-                // if (draw ===  && computer === 2 || player === 1 && computer === 1){
-                //     console.log ("Game Draw!");
-                // }      
+               playerselection === "scissor" && computerSelection === "scissor") {      
         return `It's a Draw! ${playerselection} and ${computerSelection} are the same! (Draw ${draw = d++})`;
-    } else {
-        // if (computer >= 2 || computer >= 3) {
-        //     console.log("Game Over");
-        // }                           
+    } else {                           
         return `You Lost! ${computerSelection} Beats ${playerselection} (Computer Score ${computer = c++})`;
     }
 }
@@ -68,26 +59,26 @@ function game() {
     round()
     round()
     round()
+    if (player === 5 || player === 4 && computer === 1 || player === 4 && computer === 2 ||
+        player === 4 && computer === 3 || player === 3 && computer === 1 || player === 3 && computer === 2 || 
+        player === 2 && computer === 1 || player === 4 && draw === 1 || player === 4 && draw === 2 ||
+        player === 4 && draw === 3 || player === 3 && draw === 1 || player === 3 && draw === 2 || 
+        player === 2 && draw === 1) {
+            console.log("Congratulation! You've Won!")
+    } 
+    
+    else if (computer === 5 || computer === 4 && player === 1 || computer === 4 && player === 2 ||
+             computer === 4 && player === 3 || computer === 3 && player === 1 || computer === 3 && player === 2 || 
+             computer === 2 && player === 1 || computer === 4 && draw === 1 || computer === 4 && draw === 2 ||
+             computer === 4 && draw === 3 || computer === 3 && draw === 1 || computer === 3 && draw === 2 || 
+             computer === 2 && draw === 1) {
+                console.log("Game Over, Better Luck Next Time!")
+               } else {
+                console.log("Draw, Try Again!")
+               }
 }
+
+
 
 console.log(game())
 
-//if (player >= 2 && computer < 2 || player <= 3 && computer < 3 || player === 4 || player = 5) {
-//    console.log("Congratulation! You have Won The Game!")
-//} else if (computer >= 2 && player < 2 || computer <= 3 && player < 3 || computer = 4 || computer = 5) {
-//    console.log("Game Over, You lost!")
-//} else {
-//    console.log("Draw! try again!")
-//}
-
-
-// problem: how to compare the points, so that, whenever one wins 3 time the the game announces the winner 
-//          and stops further execution of the game?
-// 
-// algorithm: 
-// 
-// 1. give points to the three player, computer and Draw elements whenever the condition for assigning the 
-//    points to the respactive variable is true,
-// 
-// 2. inside the function first run one round of game, then right below it in the if statment condition cheak
-//    if the specified condition is true if it is the in will be true and any other 
