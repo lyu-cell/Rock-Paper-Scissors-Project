4790606010c26b7bf46f1c71113c882432b4e641
lyu-cell <shizueizawa340@gmail.com>
2023-08-07 01:05:49 +0600

    Loops and static ui added

--- index.js
+++ index.js
@@ -17,25 +17,20 @@ let computerSelection = getComputerchoice();
 console.log(playerselection);
 console.log(computerSelection);
 
-let p = 1;
-let c = 1;
-let d = 1;
-
-let player = p;
-let computer = c; 
-let draw = d;
+let playerScore = 0;
+let computerScore = 0;
 
 function playRound(playerselection, computerSelection) {
     if (playerselection === "rock" && computerSelection === "scissor" ||
         playerselection === "paper" && computerSelection === "rock" ||
         playerselection === "scissor" && computerSelection === "paper") {
-        return `You Won! ${playerselection} Beats ${computerSelection} (Player Score ${player = p++})`;  
+        return `You Won! ${playerselection} Beats ${computerSelection} (Player Score ${playerScore += 1})`;  
     } else if (playerselection === "rock" && computerSelection === "rock" ||
                playerselection === "paper" && computerSelection === "paper" ||
                playerselection === "scissor" && computerSelection === "scissor") {      
-        return `It's a Draw! ${playerselection} and ${computerSelection} are the same! (Draw ${draw = d++})`;
+        return `It's a Draw! ${playerselection} and ${computerSelection} are the same!)`;
     } else {                           
-        return `You Lost! ${computerSelection} Beats ${playerselection} (Computer Score ${computer = c++})`;
+        return `You Lost! ${computerSelection} Beats ${playerselection} (Computer Score ${computerScore += 1})`;
     }
 }
 
@@ -54,28 +49,16 @@ function round() {
 
 
 
-function game() {
-    round()
-    round()
-    round()
-    round()
-    if (player === 5 || player === 4 && computer === 1 || player === 3 && computer === 2 ||
-        player === 2 && computer === 1 && draw === 2 || player === 3 && computer === 1 && draw === 1 ||
-        player === 1 && draw === 4 || player === 2 && draw === 3 || player === 3 && draw === 2 || 
-        player === 4 && draw === 1) {
-            console.log("Congratulation! You've Won!")
-    } 
+for (;playerScore <= 4;) {
     
-    else if (computer === 5 || computer === 4 && player === 1 || computer === 3 && player === 2 ||
-             computer === 2 && player === 1 && draw === 2 || computer === 3 && player === 1 && draw === 1 || 
-             computer === 1 && draw === 4 || computer === 2 && draw === 3 || computer === 3 &&  draw === 2 ||
-             computer === 4 && draw === 1) {
-                console.log("Game Over, Better Luck Next Time!")
-               } else {
-                console.log("Draw, Try Again!")
-               }
+    round()
+
+    if (computerScore === 5) {
+        console.log("Game Over")
+    } else if (playerScore === 5) {
+        console.log("Congratulation, You've Won!")
+    }
+
+    if (computerScore === 5) break    
 }
 
-
-
-console.log(game())
